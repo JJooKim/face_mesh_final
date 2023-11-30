2e4c9e96bf93da1804d565a61a6b78e2a117e22c
JJooKim <rladudwn0625@gmail.com>
2023-12-01 02:40:28 +0900

    .

--- main.py
+++ main.py
@@ -176,7 +176,7 @@ while True:
 
     with torch.no_grad():
         detect_model.eval()
-        inputs = torch.Tensor(torch.randn(19,120))
+        inputs = torch.Tensor(torch.randn(19,120)).unsqueeze(0)
         outputs = detect_model(inputs)
         print(outputs)
 
